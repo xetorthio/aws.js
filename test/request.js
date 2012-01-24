fed8a0788e3ae2a759fdaf7d35b678564d063699
Jonathan Leibiusky <ionathan@gmail.com>
2012-01-24 17:03:27 -0300

    add mocha opts and renamed test

--- test/request.js
+++ test/request.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var aws = require('../lib/aws');
 
-suite('Signature', function() {
-    test('should sign correctly', function() {
+suite('request', function() {
+    test('should be signed correctly', function() {
         var client = aws.createClient('secret', 'id', 'somehost.com');
         var signature = client.sign('/something', 'SomeAction', [], '2012-01-24T18:25:59.428Z');
         assert.equal('OUn/TVrgK+xHhyud3GgHWQdtpFMqhvpZBTa8w4Vk02M=', signature);
